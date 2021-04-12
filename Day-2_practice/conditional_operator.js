98e73a844dc09f25bf0f58cf5544494291ba75de
ashu0530 <shubham.ashutosh9@gmail.com>
2021-04-12 22:17:04 +0530

    node js practice

---
+++ Day-2_practice/conditional_operator.js
@@ -0,0 +1,46 @@
+
+
+//  < >  <= >=  !=   == ===
+
+
+
+let x =5;
+
+console.log(x<5);
+console.log(x>5);
+console.log(x==5);
+console.log(x==6);
+
+console.log(x>=5);
+console.log(x<=5);
+
+console.log(x>=6);
+console.log(x<=6);
+
+console.log(x>="6");
+console.log(x>="5");
+console.log(x<="6");
+console.log(x>="5");
+
+
+console.log(x=="5");
+console.log(x==="5");
+console.log(x=="6");
+console.log(x==="6");
+
+console.log(x!=6);
+
+console.log(x!=5);
+
+console.log(x!==5);
+console.log(x!=="5");
+console.log(x!==6);
+console.log(x!=="6");
+
+
+
+
+let y = 10;
+console.log(y += 5);     //y=y+5
+
+
